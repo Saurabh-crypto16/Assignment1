b13c4850778289d623e5194e7ebb0cd0248b3975
SAURABH PRAKASH <59662292+Saurabh-crypto16@users.noreply.github.com>
2022-08-07 20:35:39 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -14,23 +14,30 @@ const morgan = require("morgan");
 // To signup a new user and return bearer token
 router.post("/users/new", async (req, res) => {
   const { username, role } = req.body;
+  
+  //if username or role not provided then error message
   if (!username || !role) {
     return res.status(422).json({ error: "Please fill all the fields" });
   }
+  
+  //get user from database
   await User.findOne({ username: username }).then((savedUser) => {
+    //user already exists
     if (savedUser) {
       return res
         .status(422)
         .json({ error: "User with given username already exists" });
     }
+    //if user does not exists then create new user
     const user = new User({
       username: username,
       role: role,
     });
+    //save new user to database
     user
       .save()
       .then((user) => {
-        // res.json({message:"User Saved Successfully"})
+        // create jwt token and send in response
         const token = jwt.sign({ _id: user._id }, JWT_SECRET);
         res.json({ token });
       })
@@ -43,6 +50,7 @@ router.post("/users/new", async (req, res) => {
 // add new ticket
 router.post("/tickets/new", requireLogin, async (req, res) => {
   const { title, description, status, priority, assignedTo } = req.body;
+  //create ticket object
   const data = new Ticket({
     _id: uuid.v4(),
     title,
@@ -51,6 +59,7 @@ router.post("/tickets/new", requireLogin, async (req, res) => {
     priority,
     assignedTo
   });
+  //save ticket to database
   await data
     .save()
     .then((result) => {
@@ -63,12 +72,14 @@ router.post("/tickets/new", requireLogin, async (req, res) => {
 
 //show all tickets
 router.get("/tickets/all", requireLogin, async (req, res) => {
+  //fetch all tickets from database
   await Ticket.find()
     .populate(
       "detailOf",
       "_id title description status priority assignedTo createdAt"
     )
     .then((details) => {
+      //return as response
       res.json({ details });
     })
     .catch((err) => {
@@ -78,11 +89,13 @@ router.get("/tickets/all", requireLogin, async (req, res) => {
 
 //show query tickets
 router.get("/tickets/", requireLogin, async (req, res) => {
+  //fetching the query passes
   const status = req.query.status;
   const title = req.query.title;
   const priority = req.query.priority;
 
   if (status) {
+    //if status is passed in query
     await Ticket.find({ status: status })
       .populate(
         "detailOf",
@@ -95,6 +108,7 @@ router.get("/tickets/", requireLogin, async (req, res) => {
         console.log(err);
       });
   } else if (title) {
+    //if title is passed in query
     await Ticket.find({ title: title })
       .populate(
         "detailOf",
@@ -107,6 +121,7 @@ router.get("/tickets/", requireLogin, async (req, res) => {
         console.log(err);
       });
   } else if (priority) {
+    //if priority is passed in query
     await Ticket.find({ priority: priority })
       .populate(
         "detailOf",
@@ -119,6 +134,7 @@ router.get("/tickets/", requireLogin, async (req, res) => {
         console.log(err);
       });
   } else {
+    //no query passed
     res.json({ status: error });
   }
 });
@@ -127,6 +143,7 @@ router.get("/tickets/", requireLogin, async (req, res) => {
 router.post("/tickets/delete", requireLogin, async (req, res) => {
   const { ticketId } = req.body;
   if (req.user.role == "admin") {
+    //if user is admin then only allow delete
     await Ticket.findByIdAndDelete(ticketId)
       .then((result) => {
         res.json({ deletedTicket: ticketId });
@@ -143,21 +160,25 @@ router.post("/tickets/delete", requireLogin, async (req, res) => {
 //close ticket
 router.post("/tickets/markAsClosed", requireLogin, async (req, res) => {
   const { ticketId } = req.body;
-
+  
+  //fetching the required ticket
   const ticket = await Ticket.findById(ticketId)
   .catch((err) => {
     console.log(err);
   });
-
+  
+  //if the specified ticket id exists
   if (ticket) {
+    //if user is admin or ticket is assigned to that user
     if (req.user.role === "admin" || ticket.assignedTo == req.user.username) {
+      //fetch add tickets assigned to current user and having status = "open"
       await Ticket.find({ assignedTo: req.user.username, status: "open" })
         .populate(
           "detailOf",
           "_id title description status priority assignedTo createdAt"
         )
         .then((details) => {
-          //check higher priority
+          //check high or medium priority exists if so then can't close ticket 
           if (ticket.priority == "low") {
             let arr = [];
             details.forEach((detail) => {
@@ -176,6 +197,7 @@ router.post("/tickets/markAsClosed", requireLogin, async (req, res) => {
               });
             }
           }
+          //check high priority exists if so then can't close ticket
           else if (ticket.priority == "medium") {
             let arr = [];
             details.forEach((detail) => {
