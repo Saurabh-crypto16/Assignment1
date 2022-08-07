e5347b21aec3d08472b73a26f7bd75157dea3aba
SAURABH PRAKASH <59662292+Saurabh-crypto16@users.noreply.github.com>
2022-08-07 20:38:44 +0530

    Update requireLogin.js

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -4,15 +4,23 @@ const mongoose = require('mongoose')
 const User = mongoose.model("User")
 
 module.exports = (req,res,next)=>{
+    //extracting Bearers token from header
     const {authorization} = req.headers
+    
+    //if token exists
     if(!authorization){
         return res.status(401).json({error:"You must be logged in"})
     }
+    
+    //trim token
     const token = authorization.replace("Bearer ","")
+    
+    //verifying to get user from token
     jwt.verify(token,JWT_SECRET,(err,payload)=>{
         if(err){
             return res.status(401).json({error:"You must be logged in"})
         }
+        //finding id and User using that id
         const {_id} = payload
         User.findById(_id).then(userdata=>{
             req.user = userdata
