77ee3041932cf5a21a34c2724cf3f54f339a2a90
SAURABH PRAKASH <59662292+Saurabh-crypto16@users.noreply.github.com>
2022-08-07 18:29:10 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports={
-  MONGOURI = "mongodb+srv://saurabh161228:saurabh161228@cluster0.4vh7ze2.mongodb.net/?retryWrites=true&w=majority",
-  JWT_SECRET = "jkjknjksdkjsdcklmdslkwe"
+  MONGOURI: "mongodb+srv://saurabh161228:saurabh161228@cluster0.4vh7ze2.mongodb.net/?retryWrites=true&w=majority",
+  JWT_SECRET: "jkjknjksdkjsdcklmdslkwe"
 }
